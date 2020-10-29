88f39fa076247794e402713c3296e7a1b456d814
Sangha Lee <tkdgklee@gmail.com>
2020-10-28 21:01:55 -0400

    Initial Commit

---
+++ Snacktacular/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snacktacular
+//
+//  Created by Sangha Lee on 10/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
