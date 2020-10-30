2afe6d331f1f12430fd453e2f32ac29b16d20a5d
Sangha Lee <tkdgklee@gmail.com>
2020-10-29 22:13:49 -0400

    8.6

---
+++ Snacktacular/SpotTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SpotTableViewCell.swift
+//  Snacktacular
+//
+//  Created by Sangha Lee on 10/28/20.
+//
+
+import UIKit
+
+class SpotTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+}
